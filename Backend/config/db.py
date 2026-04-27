0e3f888065bd00f7dc6f180bd390c18214325faf
briankarani67 <briankarani884@gmail.com>
2026-04-27 22:54:51 +0300

    pythonized

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,14 +1,3 @@
-# import mysql.connector
-
-# def get_db_connection():
-#     db = mysql.connector.connect(
-#         host = "localhost",
-#         user = "root",
-#         password = "@Karani884",
-#         database = "testing"
-#     )
-#     return db
-
 import mysql.connector
 import os
 from dotenv import load_dotenv
