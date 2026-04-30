90d65d09ad11d6462a51914c806e2e3828dc7c80
briankarani67 <briankarani884@gmail.com>
2026-04-30 20:35:09 +0300

    Pythonized

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,14 +1,13 @@
 import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_db_connection():
     db = mysql.connector.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
+        host="localhost",
+        user="root",
+        password="@Karani884",
+        database="testing"
     )
+
+    print("CONNECTED TO:", db.database)
+
     return db
--- Backend/models/user_model.py
+++ Backend/models/user_model.py
@@ -1,18 +1,25 @@
+
 from config.db import get_db_connection
 
 def get_all_users():
     try:
         db = get_db_connection()
+
+        print("DATABASE:", db.database)
+
         cursor = db.cursor(dictionary=True)
 
         cursor.execute("SELECT * FROM users")
+
         result = cursor.fetchall()
+
+        print("RESULT:", result)
 
         cursor.close()
         db.close()
+
         return result
 
     except Exception as e:
-            print("Error:", e)
-            return []
-
+        print("Error:", e)
+        return []
