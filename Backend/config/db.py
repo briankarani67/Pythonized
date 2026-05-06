8a92d68caebe088ccfc15bef85355ed40f989b6f
briankarani67 <briankarani884@gmail.com>
2026-05-06 14:17:59 +0300

    Pythonized

--- Backend/config/db.py
+++ Backend/config/db.py
@@ -1,25 +1,12 @@
+
+
 import mysql.connector
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
 
 def get_db_connection():
-
-    host = os.getenv("DB_HOST")
-    user = os.getenv("DB_USER")
-    password = os.getenv("DB_PASSWORD")
-    database = os.getenv("DB_NAME")
-
-    print("HOST:", host)
-    print("DATABASE:", database)
-
     db = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
+        host=localhost,
+        user=root,
+        password='@Karani884',
+        database=testing
     )
-
     return db
