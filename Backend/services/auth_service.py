ce7d748d2fa7b78a7a1ad52c85a52a0583ab82c7
briankarani67 <briankarani884@gmail.com>
2026-05-11 04:20:56 +0300

    pythonized

--- Backend/services/auth_service.py
+++ Backend/services/auth_service.py
@@ -10,3 +10,12 @@ def hash_password(password):
 
     return hashed_password.decode("utf-8")
 
+def verify_password(password, hashed_password):
+    password_bin = password.encode("utf-8")
+
+    hashed_bin = hashed_password.encode("utf-8")
+
+    return bcrypt.checkpw(
+        password_bin,
+        hashed_bin
+    )
